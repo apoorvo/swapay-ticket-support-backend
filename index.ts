6d3df32101d7e9e7b380cc17ee8948b5b55bb224
Apoorvo Chakraborty <apoorvo.chak@gmail.com>
2023-04-02 19:46:39 +0530

    Update for vercel

--- index.ts
+++ index.ts
@@ -64,6 +64,4 @@ app.delete('/api/v1/tickets/:ticketid/',async (req:Request, res: Response) => {
 })
 
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+module.exports = app;
